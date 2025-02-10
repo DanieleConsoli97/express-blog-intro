13a2a94de7f8f74d4a85c2d777a0cfa947655eae
DanieleConsoli97 <Danyconsoli4@gmail.com>
2025-02-10 18:23:05 +0100

    end

--- app.js
+++ app.js
@@ -3,31 +3,31 @@ const posts = [
     {
         titolo: "Ciambellone",
         contenuto: "Un classico dolce da colazione, soffice e profumato, perfetto per iniziare la giornata.",
-        immagine: "",
+        immagine: "images/ciambellone.jpeg",
         tags: ["dolci", "colazione", "ricetta_facile"]
     },
     {
         titolo: "Cracker alla barbabietola",
         contenuto: "Cracker croccanti e colorati, realizzati con barbabietola, ideali per uno snack salutare.",
-        immagine: "",
+        immagine: "/images/cracker_barbabietola.jpeg",
         tags: ["snack", "salutare", "barbabietola"]
     },
     {
         titolo: "Pane fritto dolce",
         contenuto: "Una delizia fritta, morbida dentro e croccante fuori, da gustare come dessert.",
-        immagine: "",
+        immagine: "/images/pane_fritto_dolce.jpeg",
         tags: ["dolci", "fritto", "dessert"]
     },
     {
         titolo: "Pasta alla barbabietola",
         contenuto: "Un primo piatto originale e colorato, con una cremosa salsa a base di barbabietola.",
-        immagine: "",
+        immagine: "/images/pasta_barbabietola.jpeg",
         tags: ["primo_piatto", "barbabietola", "pasta"]
     },
     {
         titolo: "Torta paesana",
         contenuto: "Una torta rustica e golosa, tipica della tradizione contadina, con ingredienti semplici.",
-        immagine: "",
+        immagine: "/images/torta_paesana.jpeg",
         tags: ["dolci", "tradizione", "ricetta_facile"]
     }
 ];
@@ -37,6 +37,7 @@ const express = require('express')
 const app = express()
 //NOTE - porta 3000
 const port = 3000
+app.use(express.static("public"))
 //NOTE - rotta principale
 app.get('/', (req, res) => {
 //NOTE - risposta
